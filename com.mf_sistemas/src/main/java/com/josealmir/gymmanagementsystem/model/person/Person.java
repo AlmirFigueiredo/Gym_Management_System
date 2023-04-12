399138114315e721740966ee09168bca24a66bd7
Jose Almir <joseamfj@outlook.com>
2023-04-12 20:13:37 -0300

    Create instance variables and getters/setters in Person.java

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Person.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Person.java
@@ -1,5 +1,42 @@
 package com.josealmir.gymmanagementsystem.model.person;
 
 public abstract class Person {
-    
+    private String id;
+    private String fullName;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
