ef513d3f00e22ae1aee04fa45cad80ddba974a62
Jose Almir <joseamfj@outlook.com>
2023-04-12 20:16:38 -0300

    Create instance variables of the Member.java class, then getters/setters

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Member.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Member.java
@@ -1,5 +1,45 @@
 package com.josealmir.gymmanagementsystem.model.person;
 
+import java.util.Date;
+
 public class Member extends Person {
+    private String memberId;
+    private String memberShipType;
+    private Date startDate;
+    private Date endDate;
+
+    public Member(){}
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberShipType() {
+        return memberShipType;
+    }
+
+    public void setMemberShipType(String memberShipType) {
+        this.memberShipType = memberShipType;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
     
 }
