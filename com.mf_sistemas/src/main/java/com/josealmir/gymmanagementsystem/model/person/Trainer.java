39dde51356ce7ec8aa2deaceeae9c19bbd98d6e5
Jose Almir <joseamfj@outlook.com>
2023-04-12 20:18:22 -0300

    Create instance variables, constructor and getters/setters of the Trainer.java

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Trainer.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Trainer.java
@@ -1,5 +1,42 @@
 package com.josealmir.gymmanagementsystem.model.person;
 
 public class Trainer extends Person {
-    
+    private String trainerId;
+    private String speciality;
+    private Double salary;
+    private String certificationNumber;
+
+    public Trainer() {}
+
+    public String getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(String trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getCertificationNumber() {
+        return certificationNumber;
+    }
+
+    public void setCertificationNumber(String certificationNumber) {
+        this.certificationNumber = certificationNumber;
+    }
 }
