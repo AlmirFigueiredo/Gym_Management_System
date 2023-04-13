6c17d6af7c87d76be25d2f5090de374a38932a82
Jose Almir <joseamfj@outlook.com>
2023-04-12 21:08:39 -0300

    Create in Exercise class: constructor, instance vars and getters/setters

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/workoutplan/Exercise.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/workoutplan/Exercise.java
@@ -1,5 +1,44 @@
 package com.josealmir.gymmanagementsystem.model.workoutplan;
 
 public class Exercise {
+    private String name;
+    private Integer quantitySets;
+    private Integer quantityReps;
+    private Integer resTimeSeconds;
+
+    public Exercise() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantitySets() {
+        return quantitySets;
+    }
+
+    public void setQuantitySets(Integer quantitySets) {
+        this.quantitySets = quantitySets;
+    }
+
+    public Integer getQuantityReps() {
+        return quantityReps;
+    }
+
+    public void setQuantityReps(Integer quantityReps) {
+        this.quantityReps = quantityReps;
+    }
+
+    public Integer getResTimeSeconds() {
+        return resTimeSeconds;
+    }
+
+    public void setResTimeSeconds(Integer resTimeSeconds) {
+        this.resTimeSeconds = resTimeSeconds;
+    }
+
     
 }
