a4de4c499e7f4c028d1e8ca010c45ae658ba751e
Jose Almir <joseamfj@outlook.com>
2023-04-13 11:02:07 -0300

    Create First Member interface

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/MemberService.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/MemberService.java
@@ -1,5 +1,15 @@
 package com.josealmir.gymmanagementsystem.service;
 
+import java.util.LinkedList;
+
+import com.josealmir.gymmanagementsystem.model.person.Member;
+import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
+
 public interface MemberService {
-    
+    Member createMember(Member member);
+    Member findMemberById(String memberId);
+    LinkedList<Member> getAllMembers();
+    Member updateMember(String memberId, Member updatedMember);
+    WorkoutPlan updateWorkoutPlan(String memberId, WorkoutPlan updatePlan);
+    void deleteMember(String memberId);
 }
