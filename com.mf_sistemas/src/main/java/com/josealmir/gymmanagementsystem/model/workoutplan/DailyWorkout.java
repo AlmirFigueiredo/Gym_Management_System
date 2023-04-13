4de0a92c3fa19a7956e109ab2583059e97ba8ee1
Jose Almir <joseamfj@outlook.com>
2023-04-12 21:06:49 -0300

    Create in DailyWorkout class: constructor, getters/setters and instance vars

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/workoutplan/DailyWorkout.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/workoutplan/DailyWorkout.java
@@ -1,5 +1,28 @@
 package com.josealmir.gymmanagementsystem.model.workoutplan;
 
+import java.util.LinkedList;
+
 public class DailyWorkout {
+    private String dayOfWeek;
+    private LinkedList<Exercise> exercises;
+
+    public DailyWorkout() {}
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public LinkedList<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(LinkedList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
     
 }
