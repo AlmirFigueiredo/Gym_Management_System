e061dc635f311906f2f59d27b01afb2e05e9cd97
Jose Almir <joseamfj@outlook.com>
2023-04-12 21:03:47 -0300

    Create instance vars, constructor and getters/setters

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/booking/ClassReservation.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/booking/ClassReservation.java
@@ -1,5 +1,29 @@
 package com.josealmir.gymmanagementsystem.model.booking;
 
+import com.josealmir.gymmanagementsystem.model.groupfitclasses.GroupFitClass;
+import com.josealmir.gymmanagementsystem.model.person.Member;
+
 public class ClassReservation {
+    private Member member;
+    private GroupFitClass fitClass;
+    
+    public ClassReservation() {}
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public GroupFitClass getFitClass() {
+        return fitClass;
+    }
+
+    public void setFitClass(GroupFitClass fitClass) {
+        this.fitClass = fitClass;
+    }
+
     
 }
