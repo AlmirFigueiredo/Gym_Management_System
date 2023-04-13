77fab483522a2482817dca1c86c2ec99516a02e0
Jose Almir <joseamfj@outlook.com>
2023-04-13 11:06:46 -0300

    Change methods in Member Interface

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/MemberService.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/MemberService.java
@@ -6,10 +6,10 @@ import com.josealmir.gymmanagementsystem.model.person.Member;
 import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
 
 public interface MemberService {
-    Member createMember(Member member);
+    void createMember(Member member);
     Member findMemberById(String memberId);
     LinkedList<Member> getAllMembers();
-    Member updateMember(String memberId, Member updatedMember);
+    void updateMember(String memberId, Member updatedMember);
     WorkoutPlan updateWorkoutPlan(String memberId, WorkoutPlan updatePlan);
     void deleteMember(String memberId);
 }
