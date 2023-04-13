2f19e6a5ff303233c32083b784f567a64241df47
Jose Almir <joseamfj@outlook.com>
2023-04-13 11:20:57 -0300

    Start to create the MemberService implemenation

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/MemberServiceImpl.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/MemberServiceImpl.java
@@ -1,5 +1,23 @@
 package com.josealmir.gymmanagementsystem.service;
 
+import java.util.LinkedList;
+
+import com.josealmir.gymmanagementsystem.model.person.Member;
+import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
+import com.josealmir.gymmanagementsystem.service.MemberService;
 public class MemberServiceImpl implements MemberService {
-    
+    private final LinkedList<Member> members = new LinkedList<Member>();
+
+    @Override
+    public void createMember(Member member) {
+        members.add(member);
+    }
+
+    @Override
+    public LinkedList<Member> getAllMembers() {
+        LinkedList<Member> copy = new LinkedList<Member>(members);
+        return copy;
+    }
+
+
 }
