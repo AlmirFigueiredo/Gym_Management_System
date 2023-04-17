71e4e3b45536af9b1a2c286f48034a676b5ca213
Jose Almir <joseamfj@outlook.com>
2023-04-17 15:24:43 -0300

    Create Trainer Interface

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/TrainerService.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/TrainerService.java
@@ -1,5 +1,12 @@
 package com.josealmir.gymmanagementsystem.service;
 
+import com.josealmir.gymmanagementsystem.model.person.Trainer;
+import java.util.LinkedList;
+
 public interface TrainerService {
-    
+    void createMember(Trainer trainer);
+    Trainer findTrainerById(String memberId);
+    LinkedList<Trainer> getAllTrainer();
+    boolean updateTrainer(String trainerId, Trainer updateTrainer);
+    boolean deleteMember(String memberId);
 }
