e6c42fc749d56f72ce12c079e470a2cf12b432c4
Jose Almir <joseamfj@outlook.com>
2023-04-17 15:32:08 -0300

    Create inicial method

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/TrainerServiceImpl.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/TrainerServiceImpl.java
@@ -1,5 +1,40 @@
 package com.josealmir.gymmanagementsystem.service;
 
+import java.util.LinkedList;
+
+import com.josealmir.gymmanagementsystem.model.person.Trainer;
+
 public class TrainerServiceImpl implements TrainerService{
+    private LinkedList<Trainer> trainers = new LinkedList<Trainer>();
+
+    @Override
+    public void createTrainer(Trainer trainer) {
+        trainers.add(trainer);
+    }
+
+    @Override
+    public Trainer findTrainerById(String memberId) {
+
+       
+    }
+
+    @Override
+    public LinkedList<Trainer> getAllTrainer() {
+        
+        
+    }
+
+    @Override
+    public boolean updateTrainer(String trainerId, Trainer updateTrainer) {
+        
+       
+    }
+
+    @Override
+    public boolean deleteTrainer(String memberId) {
+        
+        
+    }
+    
     
 }
