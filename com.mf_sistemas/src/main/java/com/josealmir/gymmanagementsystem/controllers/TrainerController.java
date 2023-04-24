7bdc35e0949627c6826b72ccd1b5d50bbf522347
Jose Almir <joseamfj@outlook.com>
2023-04-24 19:33:28 -0300

    Create get all trainers in trainer controller

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/TrainerController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/TrainerController.java
@@ -1,9 +1,15 @@
 package com.josealmir.gymmanagementsystem.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.josealmir.gymmanagementsystem.model.person.Trainer;
 import com.josealmir.gymmanagementsystem.service.interfaces.TrainerService;
 
 @RestController
@@ -11,4 +17,9 @@ import com.josealmir.gymmanagementsystem.service.interfaces.TrainerService;
 public class TrainerController {
     @Autowired
     private TrainerService trainerService;
+
+    @GetMapping
+    public ResponseEntity<List<Trainer>> getAllTrainers() {
+        return new ResponseEntity<List<Trainer>>(trainerService.allTrainers(), HttpStatus.OK);
+    }
 }
