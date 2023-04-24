d0f6abe68ae171e57ee7350e92f0289daa964671
Jose Almir <joseamfj@outlook.com>
2023-04-24 20:01:08 -0300

    Create getmapping of get trainer by trainer id in Trainer controller

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/TrainerController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/TrainerController.java
@@ -1,11 +1,13 @@
 package com.josealmir.gymmanagementsystem.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,9 @@ public class TrainerController {
         Double salary = trainerRequest.getSalary();
         String certificationNumber = trainerRequest.getCertificationNumber();
         return trainerService.createTrainer(trainerId, speciality, salary, certificationNumber);
+    }
+    @GetMapping("/{trainerId}")
+    public ResponseEntity<Optional<Trainer>> getTrainerById(@PathVariable String trainerId) {
+        return new ResponseEntity<Optional<Trainer>>(trainerService.trainerById(trainerId), HttpStatus.OK);
     }
 }
