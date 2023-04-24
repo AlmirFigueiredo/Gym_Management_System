3d4ecda215d23b9b4e39e345ffaaf1d4f69b981f
Jose Almir <joseamfj@outlook.com>
2023-04-24 13:57:08 -0300

    Change createMember method and delete mongotemplate

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/implementations/MemberServiceImpl.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/implementations/MemberServiceImpl.java
@@ -3,7 +3,6 @@ package com.josealmir.gymmanagementsystem.service.implementations;
 import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import com.josealmir.gymmanagementsystem.model.person.Member;
 import com.josealmir.gymmanagementsystem.repositories.MemberRepository;
@@ -13,8 +12,6 @@ import com.josealmir.gymmanagementsystem.service.interfaces.MemberService;
 public class MemberServiceImpl implements MemberService {
     @Autowired
     private MemberRepository memberRepository;
-    @Autowired
-    private MongoTemplate mongoTemplate;
     @Override
     public Member createMember(String memberId, String membershipType, Date startDate, Date endDate) {
         Member member = memberRepository.insert(new Member(memberId, membershipType, startDate, endDate));
--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/interfaces/MemberService.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/interfaces/MemberService.java
@@ -3,7 +3,6 @@ package com.josealmir.gymmanagementsystem.service.interfaces;
 import java.sql.Date;
 
 import com.josealmir.gymmanagementsystem.model.person.Member;
-import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
 
 public interface MemberService {
     Member createMember(String memberId, String membershipType, Date startDate, Date endDate);
