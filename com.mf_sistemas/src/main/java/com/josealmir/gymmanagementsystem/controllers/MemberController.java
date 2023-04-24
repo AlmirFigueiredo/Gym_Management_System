118e38de162ad627951286f7b03da975cc56cfca
Jose Almir <joseamfj@outlook.com>
2023-04-24 14:24:12 -0300

    Create MemberRequest and modify the createMember method in MemberController class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
@@ -1,15 +1,14 @@
 package com.josealmir.gymmanagementsystem.controllers;
 
-import java.util.Map;
 import java.sql.Date;
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.josealmir.gymmanagementsystem.model.person.Member;
+import com.josealmir.gymmanagementsystem.requests.MemberRequest;
 import com.josealmir.gymmanagementsystem.service.interfaces.MemberService;
 
 @RestController
@@ -18,4 +17,12 @@ public class MemberController {
     @Autowired
     private MemberService memberService;    
     
+    @PostMapping
+    public Member createMember(@RequestBody MemberRequest memberRequest) {
+        String memberId = memberRequest.getMemberId();
+        String membershipType = memberRequest.getMembershipType();
+        Date startDate = memberRequest.getStartDate();
+        Date endDate = memberRequest.getEndDate();
+        return memberService.createMember(memberId, membershipType, startDate, endDate);
+    }
 }
--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/MemberRequest.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/MemberRequest.java
@@ -1,8 +1,13 @@
 package com.josealmir.gymmanagementsystem.requests;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class MemberRequest {
-    
+    private String memberId;
+    private String membershipType;
+    private Date startDate;
+    private Date endDate;
 }
