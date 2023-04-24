525ebd5483930067351e7a310d92153df9243f8f
Jose Almir <joseamfj@outlook.com>
2023-04-24 11:17:56 -0300

    Start Trainer repository file

---
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/repositories/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.josealmir.gymmanagementsystem.repositories;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.josealmir.gymmanagementsystem.model.person.Trainer;
+
+public interface TrainerRepository extends MongoRepository<Trainer, ObjectId>{
+    
+}
