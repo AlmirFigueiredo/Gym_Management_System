f695bb3e9bd3a466b79fea194cc4cf738f791722
Jose Almir <joseamfj@outlook.com>
2023-04-24 11:15:52 -0300

    Modify Member Controller, to use MemberService instead of Member

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
@@ -3,13 +3,13 @@ package com.josealmir.gymmanagementsystem.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.josealmir.gymmanagementsystem.model.person.Member;
+import com.josealmir.gymmanagementsystem.service.interfaces.MemberService;
 
 @RestController
 @RequestMapping("/Members")
 public class MemberController {
     @Autowired
-    private Member member;
+    private MemberService member;
+    
     
 }
