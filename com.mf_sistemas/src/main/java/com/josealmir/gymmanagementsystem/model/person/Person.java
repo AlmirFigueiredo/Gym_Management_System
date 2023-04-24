4be273d0975b30ff2091950a40d65fee995fec76
Jose Almir <joseamfj@outlook.com>
2023-04-24 11:44:29 -0300

    Refactor Person class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Person.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Person.java
@@ -1,42 +1,16 @@
 package com.josealmir.gymmanagementsystem.model.person;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public abstract class Person {
     private String id;
     private String fullName;
     private String email;
     private String address;
     private String phoneNumber;
-
-    public Person(){}
-    
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getFullName() {
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
