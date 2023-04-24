b01f639b662e327bc5c0a22080f572e8df4e8643
Jose Almir <joseamfj@outlook.com>
2023-04-24 11:52:11 -0300

    Start TrainerController and MemberControlelr

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
@@ -12,4 +12,5 @@ public class MemberController {
     private MemberService member;
     
     
+    
 }
--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/TrainerController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/TrainerController.java
@@ -1,5 +1,14 @@
 package com.josealmir.gymmanagementsystem.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.josealmir.gymmanagementsystem.service.interfaces.TrainerService;
+
+@RestController
+@RequestMapping("/Trainers")
 public class TrainerController {
-    
+    @Autowired
+    private TrainerService trainerService;
 }
