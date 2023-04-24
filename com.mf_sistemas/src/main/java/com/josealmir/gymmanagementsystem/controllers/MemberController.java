a9990e5f4b0be560e537c9d04c9dcd3995fed789
Jose Almir <joseamfj@outlook.com>
2023-04-24 19:48:50 -0300

    Create get Member by Id in Member controller class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
@@ -2,11 +2,13 @@ package com.josealmir.gymmanagementsystem.controllers;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +36,9 @@ public class MemberController {
         Date endDate = memberRequest.getEndDate();
         return memberService.createMember(memberId, membershipType, startDate, endDate);
     }
+    @GetMapping("/{trainerId}")
+    public ResponseEntity<Optional<Member>> getMemberById(@PathVariable String memberId) {
+        return new ResponseEntity<Optional<Member>>(memberService.memberById(memberId), HttpStatus.OK);
+    }
+
 }
