63b3bedacb26c1b6d45fdccd82cac0a6e43ed728
Jose Almir <joseamfj@outlook.com>
2023-04-24 11:47:06 -0300

    Rewrite Trainer class, using lombok to simply the code

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Trainer.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Trainer.java
@@ -1,42 +1,20 @@
 package com.josealmir.gymmanagementsystem.model.person;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "trainers")
+@Data
+@EqualsAndHashCode(callSuper =  true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trainer extends Person {
     private String trainerId;
     private String speciality;
     private Double salary;
     private String certificationNumber;
-
-    public Trainer() {}
-
-    public String getTrainerId() {
-        return trainerId;
-    }
-
-    public void setTrainerId(String trainerId) {
-        this.trainerId = trainerId;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setSpeciality(String speciality) {
-        this.speciality = speciality;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    public String getCertificationNumber() {
-        return certificationNumber;
-    }
-
-    public void setCertificationNumber(String certificationNumber) {
-        this.certificationNumber = certificationNumber;
-    }
 }
