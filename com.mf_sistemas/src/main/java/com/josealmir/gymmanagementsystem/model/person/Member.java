599ebc8c951e15655e2e2c242578baf4cfbd804a
Jose Almir <joseamfj@outlook.com>
2023-04-24 11:42:15 -0300

    Refactoring Member class, to include the mongo db

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Member.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/person/Member.java
@@ -1,56 +1,26 @@
 package com.josealmir.gymmanagementsystem.model.person;
 
 import java.util.Date;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Document(collection = "members")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member extends Person {
-
+    @Id
     private String memberId;
     private String memberShipType;
-    private WorkoutPlan workoutPlan;
     private Date startDate;
     private Date endDate;
-
-    public Member(){}
-
-    public String getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(String memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getMemberShipType() {
-        return memberShipType;
-    }
-
-    public void setMemberShipType(String memberShipType) {
-        this.memberShipType = memberShipType;
-    }
-
-    public WorkoutPlan getWorkoutPlan() {
-        return workoutPlan;
-    }
-    public void setWorkoutPlan(WorkoutPlan workoutPlan) {
-        this.workoutPlan = workoutPlan;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-    
+    @DocumentReference
+    private WorkoutPlan workoutPlan;    
 }
