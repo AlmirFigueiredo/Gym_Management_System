8125ae6a9e69c9c5fb61a3a5a0e6ca9341bf36c8
Jose Almir <joseamfj@outlook.com>
2023-04-25 09:45:46 -0300

    Delete member by Id tested and it's working

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/implementations/MemberServiceImpl.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/service/implementations/MemberServiceImpl.java
@@ -4,7 +4,6 @@ import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.josealmir.gymmanagementsystem.model.person.Member;
