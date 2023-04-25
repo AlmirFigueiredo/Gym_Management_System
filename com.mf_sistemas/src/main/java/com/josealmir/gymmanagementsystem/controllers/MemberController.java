0367d124c9a4c1e0a5a842f9e14d36aee1dab850
Jose Almir <joseamfj@outlook.com>
2023-04-25 09:30:34 -0300

    Create deleteMember mapping in Member Controller

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/MemberController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,10 @@ public class MemberController {
     public ResponseEntity<Optional<Member>> getMemberById(@PathVariable String memberId) {
         return new ResponseEntity<Optional<Member>>(memberService.memberById(memberId), HttpStatus.OK);
     }
-    
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMember(@PathVariable String memberId) {
+        memberService.deleteMemberById(memberId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
     
 }
