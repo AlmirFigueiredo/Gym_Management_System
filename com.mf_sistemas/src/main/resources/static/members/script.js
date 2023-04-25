9f8e2a51640f4c28ab18795f7c5a584f920e792a
Jose Almir <joseamfj@outlook.com>
2023-04-25 19:01:29 -0300

    Start fetchMembers function

--- com.mf_sistemas/src/main/resources/static/members/script.js
+++ com.mf_sistemas/src/main/resources/static/members/script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     fetchMembers();
 });
+
+function fetchMembers() {
+    fetch("/Members")
+        .then(response => response.json())
+        .then(members => {
+            displayMembers(members);
+        })
+        .catch(error => {
+            console.error('Error fetching members:', error);
+        });
+}
