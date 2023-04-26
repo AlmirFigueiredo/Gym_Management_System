d089bfc25fb3e568cf0bb13cfd1eed2c0880ca3f
Jose Almir <joseamfj@outlook.com>
2023-04-26 16:53:17 -0300

    Fix type in add_membersjs file and move the button to form block in html file

--- com.mf_sistemas/src/main/resources/static/members/add_member/add_member.js
+++ com.mf_sistemas/src/main/resources/static/members/add_member/add_member.js
@@ -28,7 +28,7 @@ function submitForm(event) {
     const startDate = document.getElementById('startDate').value;
     const endDate = document.getElementById('endDate').value
     const email = document.getElementById('email').value;
-    const address = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
     const phoneNumber = document.getElementById('phoneNumber').value;
     
     const newMember = {
