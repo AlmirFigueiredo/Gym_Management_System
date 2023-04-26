652092005876727ce3662801bed2929be499e0da
Jose Almir <joseamfj@outlook.com>
2023-04-26 09:47:59 -0300

    Fetch trainers and display in scriptjs

---
+++ com.mf_sistemas/src/main/resources/static/trainers/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetchTrainers();
+})
+
+function fetchTrainers() {
+    fetch("/Trainers")
+        .then(response => response.json())
+        .then(trainers => {
+            displayTrainers(trainers);
+        })
+        .catch(error => {
+            console.error('Error fetching trainers: ', error);
+        });
+}
+
+function displayTrainers(trainers) {
+    const trainerTableBody = document.querySelector('#trainerTable tbody');
+    trainerTableBody.innerHTML = '';
+    trainers.forEach(trainer => {
+        const row = document.createElement('tr');
+
+        const trainerIdCell = document.createElement('td');
+        trainerIdCell.textContent = trainer.trainerId;
+        row.appendChild(trainerIdCell);
+
+        const trainerNameCell = document.createElement('td');
+        trainerNameCell.textContent = trainer.fullName;
+        row.appendChild(trainerNameCell);
+
+        const trainerSpecialityCell = document.createElement('td');
+        trainerSpecialityCell.textContent = trainer.speciality;
+        row.appendChild(trainerSpecialityCell);
+
+        const trainerCertificationNumberCell = document.createElement('td');
+        trainerCertificationNumberCell.textContent = trainer.certificationNumber;
+        row.appendChild(trainerCertificationNumberCell);
+
+        trainerTableBody.appendChild(row);
+    })
+}
