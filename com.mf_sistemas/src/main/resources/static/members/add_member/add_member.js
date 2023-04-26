5b10744718e6730fd9d7c7f098524276df0d3936
Jose Almir <joseamfj@outlook.com>
2023-04-26 16:14:46 -0300

    In add_memberjs create function to submitform and get elements by Id

---
+++ com.mf_sistemas/src/main/resources/static/members/add_member/add_member.js
@@ -0,0 +1,13 @@
+function submitForm(event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById('fullName').value;
+    const memberShipType = document.getElementById('memberShipType').value;
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('email').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    
+    
+}
