9d47a5333f4683a57ebebb2a6a42170a484b97c2
Jose Almir <joseamfj@outlook.com>
2023-04-30 18:01:45 -0300

    Create exercise request class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/ExerciseRequest.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/ExerciseRequest.java
@@ -1,5 +1,11 @@
 package com.josealmir.gymmanagementsystem.requests;
 
+import lombok.Data;
+
+@Data
 public class ExerciseRequest {
-    
+    private String name;
+    private Integer quantitySets;
+    private Integer quantityReps;
+    private Integer resTimeSeconds;
 }
