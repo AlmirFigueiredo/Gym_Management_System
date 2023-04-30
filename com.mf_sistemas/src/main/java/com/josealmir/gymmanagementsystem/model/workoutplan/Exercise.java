6f0c9d92a7523fb30a81232f2afb834253802d39
Jose Almir <joseamfj@outlook.com>
2023-04-30 18:00:00 -0300

    Change Exercise class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/workoutplan/Exercise.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/model/workoutplan/Exercise.java
@@ -1,44 +1,21 @@
 package com.josealmir.gymmanagementsystem.model.workoutplan;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "exercises")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Exercise {
+    @Id
     private String name;
     private Integer quantitySets;
     private Integer quantityReps;
-    private Integer resTimeSeconds;
-
-    public Exercise() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getQuantitySets() {
-        return quantitySets;
-    }
-
-    public void setQuantitySets(Integer quantitySets) {
-        this.quantitySets = quantitySets;
-    }
-
-    public Integer getQuantityReps() {
-        return quantityReps;
-    }
-
-    public void setQuantityReps(Integer quantityReps) {
-        this.quantityReps = quantityReps;
-    }
-
-    public Integer getResTimeSeconds() {
-        return resTimeSeconds;
-    }
-
-    public void setResTimeSeconds(Integer resTimeSeconds) {
-        this.resTimeSeconds = resTimeSeconds;
-    }
-
-    
+    private Integer resTimeSeconds;    
 }
