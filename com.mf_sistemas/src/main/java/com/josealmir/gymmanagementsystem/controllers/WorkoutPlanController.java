2422595514d0d874a3a1edeb91a3525dbf816846
Jose Almir <joseamfj@outlook.com>
2023-04-29 22:48:09 -0300

    Create getMapping of all workoutplans

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/WorkoutPlanController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/WorkoutPlanController.java
@@ -1,5 +1,25 @@
 package com.josealmir.gymmanagementsystem.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
+import com.josealmir.gymmanagementsystem.service.interfaces.WorkoutPlanService;
+
+@RestController
+@RequestMapping("/WorkoutPlans")
 public class WorkoutPlanController {
+    @Autowired
+    private WorkoutPlanService workoutPlanService;
     
+    @GetMapping
+    public ResponseEntity<List<WorkoutPlan>> getAllWorkoutPlans() {
+        return new ResponseEntity<List<WorkoutPlan>>(workoutPlanService.allWorkoutPlans(), HttpStatus.OK);
+    } 
 }
