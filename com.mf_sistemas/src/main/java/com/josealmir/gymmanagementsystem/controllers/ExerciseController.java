0f97fb5a51a0156e8df18d7c4890a3e9296b33aa
Jose Almir <joseamfj@outlook.com>
2023-04-30 18:23:35 -0300

    Create delete mapping for exercise class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/ExerciseController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/ExerciseController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,10 @@ public class ExerciseController {
     public ResponseEntity<Optional<Exercise>> getExerciseByName(@PathVariable String name) {
         return new ResponseEntity<Optional<Exercise>>(exerciseService.exerciseByName(name), HttpStatus.OK);
     }
-    
+    @DeleteMapping
+    public ResponseEntity<Void> deleteExercise(@PathVariable String name) {
+        exerciseService.deleteExerciseByName(name);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
