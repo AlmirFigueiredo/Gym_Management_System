c49c38c26ddb137b2c74542d51f2e7d471d042be
Jose Almir <joseamfj@outlook.com>
2023-04-30 10:37:06 -0300

    Create dailyWorkoutRequest class

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/DailyWorkoutController.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/controllers/DailyWorkoutController.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.josealmir.gymmanagementsystem.model.workoutplan.DailyWorkout;
+import com.josealmir.gymmanagementsystem.model.workoutplan.WorkoutPlan;
 import com.josealmir.gymmanagementsystem.service.interfaces.DailyWorkoutService;
 
 @RestController
@@ -24,5 +27,9 @@ public class DailyWorkoutController {
         return new ResponseEntity<List<DailyWorkout>>(dailyWorkoutService.allDailyWorkouts(), HttpStatus.OK);
     }
 
-    
+    @PostMapping
+    public WorkoutPlan createWorkoutPlan(@RequestBody DailyWorkout dailyWorkout) {
+        
+    }
+
 }
--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/DailyWorkoutRequest.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/DailyWorkoutRequest.java
@@ -1,5 +1,13 @@
 package com.josealmir.gymmanagementsystem.requests;
 
+import java.util.List;
+
+import com.josealmir.gymmanagementsystem.model.workoutplan.Exercise;
+
+import lombok.Data;
+
+@Data
 public class DailyWorkoutRequest {
-    
+    private String dayOfWeek;
+    private List<Exercise> exercises;
 }
