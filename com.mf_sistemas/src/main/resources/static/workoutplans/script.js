149505ab2d5ff9787cdeaffc0c2ebae6911d308d
Jose Almir <joseamfj@outlook.com>
2023-05-01 18:54:43 -0300

    Change script in workoutplan

--- com.mf_sistemas/src/main/resources/static/workoutplans/script.js
+++ com.mf_sistemas/src/main/resources/static/workoutplans/script.js
@@ -4,12 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function fetchWorkoutPlans() {
     fetch("/WorkoutPlans")
-        .then(response => response.json())
-        .then(workoutPlans => {
-            displayWorkoutPlans(workoutPlans);
+        .then((response) => response.json())
+        .then(async (workoutPlans) => {
+            const workoutPlansWithDetails = await Promise.all(
+                workoutPlans.map(async (workoutPlan) => {
+                    const memberResponse = await fetch(`/Members/${workoutPlan.memberId}`);
+                    const member = await memberResponse.json();
+                    const trainerResponse = await fetch(`/Trainers/${workoutPlan.trainerId}`);
+                    const trainer = await trainerResponse.json();
+                    return { ...workoutPlan, member, trainer };
+                })
+            );
+            displayWorkoutPlans(workoutPlansWithDetails);
         })
-        .catch(error => {
-            console.error('Error fetching members:', error);
+        .catch((error) => {
+            console.error("Error fetching:", error);
         });
 }
 
@@ -20,17 +29,17 @@ function displayWorkoutPlans(workoutPlans) {
         const row = document.createElement('tr');
 
         const memberIdCell = document.createElement('td');
-        memberIdCell.textContent = workoutPlan.member.memberId;
+        memberIdCell.textContent = workoutPlan.memberId ? workoutPlan.memberId : 'N/A';
         row.appendChild(memberIdCell);
 
         const memberNameCell = document.createElement('td');
-        memberNameCell.textContent = workoutPlan.member.fullName;
+        memberNameCell.textContent = workoutPlan.member ? workoutPlan.member.fullName : 'N/A';
         row.appendChild(memberNameCell);
 
         const trainerIdCell = document.createElement('td');
-        trainerIdCell.textContent = workoutPlan.trainer.trainerId;
+        trainerIdCell.textContent = workoutPlan.trainerId ? workoutPlan.trainerId : 'N/A';
         row.appendChild(trainerIdCell);
-        
+
         const startDate = document.createElement('td');
         startDate.textContent = workoutPlan.startDate;
         row.appendChild(startDate);
@@ -38,7 +47,7 @@ function displayWorkoutPlans(workoutPlans) {
         const endDate = document.createElement('td');
         endDate.textContent = workoutPlan.endDate;
         row.appendChild(endDate);
-        
+
         row.classList.add('table-row-button');
         row.addEventListener('click', () => {
             window.location.href = `workoutplan_details/workoutplan_details.html?workoutPlanId=${workoutPlan.id}`;
@@ -47,3 +56,4 @@ function displayWorkoutPlans(workoutPlans) {
         workoutPlanTable.appendChild(row);
     });
 }
+
