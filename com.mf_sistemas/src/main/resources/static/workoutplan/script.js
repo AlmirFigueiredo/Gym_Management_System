74ba3dd37ae7863147ec61bb42f4b2095dae9748
Jose Almir <joseamfj@outlook.com>
2023-04-30 21:49:51 -0300

    Start workoutplans webpage

---
+++ com.mf_sistemas/src/main/resources/static/workoutplan/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetchWorkoutPlans();
+});
+
+function fetchWorkoutPlans() {
+    fetch("/WorkoutPlans")
+        .then(response => response.json())
+        .then(workoutPlans => {
+            displayWorkoutPlans(workoutPlans);
+        })
+        .catch(error => {
+            console.error('Error fetching members:', error);
+        });
+}
+
+function displayWorkoutPlans(workoutPlans) {
+    const workoutPlanTable = document.querySelector('#workoutPlanTable tbody');
+    workoutPlanTable.innerHTML = '';
+    workoutPlans.forEach(workoutPlan => {
+        const row = document.createElement('tr');
+
+        const memberIdCell = document.createElement('td');
+        memberIdCell.textContent = workoutPlan.member.memberId;
+        row.appendChild(memberIdCell);
+
+        const memberNameCell = document.createElement('td');
+        memberNameCell.textContent = workoutPlan.member.fullName;
+        row.appendChild(memberNameCell);
+
+        const startDate = document.createElement('td');
+        startDate.textContent = workoutPlan.startDate;
+        row.appendChild(startDate);
+
+        const endDate = document.createElement('td');
+        endDate.textContent = workoutPlan.endDate;
+        row.appendChild(endDate);
+        
+        workoutPlanTable.appendChild(row);
+    });
+}
