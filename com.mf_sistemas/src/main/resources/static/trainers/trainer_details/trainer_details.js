d2f2c3eb892073fbf627e83d70595863739fef96
Jose Almir <joseamfj@outlook.com>
2023-05-08 08:55:24 -0300

    Create trainder details page and fetch the trainer data

---
+++ com.mf_sistemas/src/main/resources/static/trainers/trainer_details/trainer_details.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const trainerId = urlParams.get('trainerId');
+
+    if (trainerId) {
+        fetchTrainerDetails(trainerId);
+    } else {
+        window.location.href = '../index.html';
+    }
+});
+
+function fetchTrainerDetails(trainerId) {
+    fetch(`/Trainers/${trainerId}`)
+        .then(response => response.json())
+        .then(trainer => {
+            displayTrainerDetails(trainer);
+        })
+        .catch(error => {
+            console.error('Error fetching trainer details:', error);
+        });
+}
+
+function displayTrainerDetails(trainer) {
+    document.querySelector('#trainerId').textContent = trainer.trainerId;
+    document.querySelector('#fullName').textContent = trainer.fullName;
+    document.querySelector('#salary').textContent = trainer.salary; // Fixed
+    document.querySelector('#certificationNumber').textContent = trainer.certificationNumber; // Fixed
+    document.querySelector('#email').textContent = trainer.email;
+    document.querySelector('#address').textContent = trainer.address;
+    document.querySelector('#phoneNumber').textContent = trainer.phoneNumber;
+    document.querySelector('#speciality').textContent = trainer.speciality;
+}
