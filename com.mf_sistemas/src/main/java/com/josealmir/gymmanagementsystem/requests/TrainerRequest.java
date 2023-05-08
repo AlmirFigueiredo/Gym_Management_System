d3b2110811d286090c8d2164cbc67a76211ac677
Jose Almir <joseamfj@outlook.com>
2023-05-08 08:17:53 -0300

    Create trainer details page

--- com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/TrainerRequest.java
+++ com.mf_sistemas/src/main/java/com/josealmir/gymmanagementsystem/requests/TrainerRequest.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class TrainerRequest {
-    private String fullName;
+    private String fullName; 
     private String email;
-    private String phoneNumber;
-    private String address;
-    private String trainerId;
-    private String speciality;
-    private Double salary;
+    private String phoneNumber; 
+    private String address; 
+    private String trainerId; 
+    private String speciality; 
+    private Double salary; 
     private String certificationNumber;
 }
