1ede5d14030108b3189c5af787db00dd08a48793
Jose Almir <joseamfj@outlook.com>
2023-05-29 12:01:29 -0300

    Create edit trainer fecthing

--- com.mf_sistemas/src/main/resources/static/trainers/edit_trainer/edit_trainer.js
+++ com.mf_sistemas/src/main/resources/static/trainers/edit_trainer/edit_trainer.js
@@ -5,7 +5,7 @@ async function fetchTrainerDetails(trainerId) {
         const response = await fetch(`/Trainers/${trainerId}`);
         const trainer = await response.json();
         displayTrainerDetais(trainer);
-    } catch(error) {
+    } catch (error) {
         console.error('Error fetching trainer details', error);
     }
 }
@@ -19,3 +19,62 @@ function displayTrainerDetais(trainer) {
     document.getElementById('salary').value = trainer.salary;
     document.getElementById('certificationNumber').value = trainer.certificationNumber;
 }
+
+async function updateTrainer(trainerInfo) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const trainerId = urlParams.get('trainerId');
+    try {
+        const response = await fetch(`/Trainers/${trainerId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(trainerInfo),
+        });
+        if (response.ok) {
+            alert('Trainer updated successfully');
+        } else {
+            alert('Failed to update trainer');
+        }
+    } catch (error) {
+        console.error('Error updating trainer:', error);
+    }
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById('fullName').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const speciality = document.getElementById('speciality').value;
+    const salary = document.getElementById('salary').value;
+    const certificationNumber = document.getElementById('certificationNumber').value;
+
+
+    const newTrainer = {
+        fullName: fullName,
+        email: email,
+        address: address,
+        phoneNumber: phoneNumber,
+        speciality: speciality,
+        salary: salary,
+        certificationNumber: certificationNumber
+    }
+    updateTrainer(newTrainer).then(() => {
+        window.location.href = '../index.html'
+    });
+    event.target.reset();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const trainerId = urlParams.get('trainerId');
+
+    if (trainerId) {
+        fetchTrainerDetails(trainerId);
+    } else {
+        window.location.href = '../index.html';
+    }
+});
