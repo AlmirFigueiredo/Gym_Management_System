2b1b4a5f147247edc07223901f3e502e08bf4ad8
Jose Almir <joseamfj@outlook.com>
2023-05-29 11:50:21 -0300

    Create displayTrainers function in edit trainers page

--- com.mf_sistemas/src/main/resources/static/trainers/edit_trainer/edit_trainer.js
+++ com.mf_sistemas/src/main/resources/static/trainers/edit_trainer/edit_trainer.js
@@ -6,6 +6,16 @@ async function fetchTrainerDetails(trainerId) {
         const trainer = await response.json();
         displayTrainerDetais(trainer);
     } catch(error) {
-        console.error('Error fetching trainer details', error)
+        console.error('Error fetching trainer details', error);
     }
+}
+
+function displayTrainerDetais(trainer) {
+    document.getElementById('fullName').value = trainer.fullName;
+    document.getElementById('email').value = trainer.email;
+    document.getElementById('phoneNumber').value = trainer.phoneNumber;
+    document.getElementById('address').value = trainer.address;
+    document.getElementById('speciality').value = trainer.speciality;
+    document.getElementById('salary').value = trainer.salary;
+    document.getElementById('certificationNumber').value = trainer.certificationNumber;
 }
