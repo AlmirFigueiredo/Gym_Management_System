42b2a7289bf3ad660b9370de04a881c4f37c6e2c
Jose Almir <joseamfj@outlook.com>
2023-05-30 14:06:34 -0300

    Create js file to in dailyworkout page

--- com.mf_sistemas/src/main/resources/static/workoutplans/dailyWorkout/dailyWorkout.js
+++ com.mf_sistemas/src/main/resources/static/workoutplans/dailyWorkout/dailyWorkout.js
@@ -2,24 +2,74 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const day = urlParams.get('day');
     fetchDailyWorkout(day);
+
+    const modalBox = document.getElementById('modalBox');
+    const closeSpan = document.getElementsByClassName('close')[0];
+    const saveButton = document.getElementById('saveButton');
+
+    closeSpan.onclick = function () {
+        modalBox.style.display = 'none';
+    }
+    window.onclick = function (event) {
+        if (event.target == modalBox) {
+            modalBox.style.display = 'none';
+        }
+    }
+    saveButton.onclick = modifyExercise;
 });
 
 async function fetchDailyWorkout(day) {
-    // Fetch the workout plan for this day
     const response = await fetch(`/WorkoutPlans?day=${day}`);
     const dailyWorkout = await response.json();
-
-    // Display the workout plan on the page
     displayDailyWorkout(dailyWorkout);
 }
 
 function displayDailyWorkout(dailyWorkout) {
-    // Code to display the workout plan goes here. 
-    // This will be similar to the displayWorkoutPlanDetails function, but for a single day's plan.
+    const tbody = document.getElementById('workoutTable').getElementsByTagName('tbody')[0];
+    dailyWorkout.exercises.forEach(exercise => {
+        let row = tbody.insertRow();
+        row.insertCell().innerHTML = exercise.name;
+        row.insertCell().innerHTML = exercise.quantitySets;
+        row.insertCell().innerHTML = exercise.quantityReps;
+        row.insertCell().innerHTML = exercise.resTimeSeconds;
+        let cell = row.insertCell();
+        let editButton = document.createElement('button');
+        editButton.innerText = 'Edit';
+        editButton.addEventListener('click', function () {
+            showEditModal(exercise.id);
+        });
+        cell.appendChild(editButton);
+    });
 }
 
-// Add event listener for the edit button
-const editButton = document.getElementById('editButton');
-editButton.addEventListener('click', function () {
-    // Code to edit the workout plan goes here
-});
+function showEditModal(exerciseId) {
+    fetch(`/Exercises/${exerciseId}`)
+        .then(response => response.json())
+        .then(exercise => {
+            document.getElementById('name').value = exercise.name;
+            document.getElementById('quantitySets').value = exercise.quantitySets;
+            document.getElementById('quantityReps').value = exercise.quantityReps;
+            document.getElementById('resTimeSeconds').value = exercise.resTimeSeconds;
+            document.getElementById('modalBox').style.display = 'block';
+        });
+}
+
+async function modifyExercise() {
+    const exerciseId = document.getElementById('exerciseId').value;
+    const response = await fetch(`/Exercises/${exerciseId}`);
+    const exercise = await response.json();
+    const newExercise = { ...exercise };
+    newExercise.name = document.getElementById('name').value;
+    newExercise.quantitySets = document.getElementById('quantitySets').value;
+    newExercise.quantityReps = document.getElementById('quantityReps').value;
+    newExercise.resTimeSeconds = document.getElementById('resTimeSeconds').value;
+    await fetch('/Exercises', {
+        method: 'POST',
+        body: JSON.stringify(newExercise),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    document.getElementById('modalBox').style.display = 'none';
+    fetchDailyWorkout(newExercise.day);
+}
